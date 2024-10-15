e13d5932e60fb5993fdb2dc95d310b400981a9ee
kelseaconrad19 <163567548+kelseaconrad19@users.noreply.github.com>
2024-10-15 11:04:57 -0400

    changed line 38 of new app for find_all sums

--- new_app.py
+++ new_app.py
@@ -35,7 +35,7 @@ sums_schema = SumSchema()
 @new_app.route('/sum', methods=['GET'])
 def find_all():
     sums = db.session.execute(db.select(Sum)).sclars()
-    return sums_schema.jsonify(sums), 200
+    return jsonify(sums_schema.dumps(sums, many=True)), 200
 
 @new_app.route('/sum', methods=['POST'])
 def sum():
